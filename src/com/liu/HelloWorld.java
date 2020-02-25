77e6529d152fd879ba1181e829f82c365461b434
jeromeliugit <24002400@qq.com>
2020-02-25 17:52:46 +0800

    Initial commit

---
+++ src/com/liu/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.liu;
+
+import java.util.ArrayList;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, world1!");
+        ArrayList al = new ArrayList();
+        String str = new String();
+        System.out.println("aaa");
+
+    }
+
+
+}
